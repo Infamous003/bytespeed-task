b98622bf58bc61968747b919a3dc8c461c343858
Infamous003 <arfathsyed0312@gmail.com>
2025-07-12 12:37:38 +0530

    Add Contact model and setup SQLite database

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from database import create_db_and_tables, engine
+from sqlmodel import Session, select
+from models import Contact, ContactCreate
 
 app = FastAPI()
+
+create_db_and_tables()
 
 @app.get("/")
 def root():
     return {"Hello": "World!"}
-
---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from enum import Enum
+
+class LinkPrecedence(str, Enum):
+    primary = "primary"
+    secondary = "secondary"
+
+class Contact(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    phoneNumber: str | None = Field(default=None, nullable=True)
+    email: str | None = Field(default=None, nullable=True)
+    linkedId: int | None = Field(default=None, nullable=True)
+    linkPrecedence: LinkPrecedence = Field(default=LinkPrecedence.primary)
+    createdAt: datetime = Field(default_factory=datetime.now)
+    updatedAt: datetime = Field(default_factory=datetime.now)
+    deletedAt: datetime | None = Field(default=None, nullable=True)
+
+class ContactCreate(BaseModel):
+    phoneNumber: str | None = None
+    email: str | None = None
