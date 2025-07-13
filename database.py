a70929c6def1733fbc1044e014ca53c581572234
Infamous003 <arfathsyed0312@gmail.com>
2025-07-13 12:15:36 +0530

    Add database session dependency

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 
 sqlite_db_name = "databse.db"
 sqlite_url = f"sqlite:///{sqlite_db_name}"
@@ -7,3 +7,7 @@ engine = create_engine(sqlite_url, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
