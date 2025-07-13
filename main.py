6c2c2841aecf9b8624f610ebfa7e61be3c1c94a0
Infamous003 <arfathsyed0312@gmail.com>
2025-07-13 16:06:23 +0530

    Extend identify endpoint to handle contact identification when matching contacts aready exist in the DB

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def root():
 @app.post("/identify")
 def identify_contact(contact: ContactCreate,
                      session: Session = Depends(get_session)):
-    
     if not contact.email and not contact.phoneNumber:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Either email or phoneNumber must be provided")
 
@@ -46,3 +45,70 @@ def identify_contact(contact: ContactCreate,
             secondaryContactIds= []
         )
         return {"contact" : contact_response}
+    
+    # When there are matched contacts
+    else:
+        new_contact = None
+        # Here, we find the oldest(primary) record
+        # assuming the first contact to be 'primary'
+        first_contact = matchedContacts[0]
+
+        # If it's primary? cool, if not we get its linked primary contact.
+        if first_contact.linkPrecedence == LinkPrecedence.primary:
+            primary_contact = first_contact
+        else:
+            query = select(Contact).where(Contact.id == first_contact.linkedId)
+            primary_contact = session.exec(query).first()
+
+        # We collect all the unique emails and phone nums from the matched contacts list
+        # Using Set() instead of List()
+        all_unique_emails = {c.email for c in matchedContacts if c.email}
+        all_unique_phones = {c.phoneNumber for c in matchedContacts if c.phoneNumber}
+
+        is_email_nonempty = contact.email != ""
+        is_phone_nonempty = contact.phoneNumber != ""
+
+        # We need to find is th euser entered any new info, i.e, a new email or phoneNum
+        # ONLY if the user provides some new info do we create a new Contact in DB
+        is_new_email = False
+        is_new_phone = False
+
+        if is_email_nonempty and contact.email not in all_unique_emails:
+            is_new_email = True
+        if is_phone_nonempty and contact.phoneNumber not in all_unique_phones:
+            is_new_phone = True
+        
+        should_create_new_model = False
+        if is_new_email == True or is_new_phone == True:
+            should_create_new_model = True
+
+        if should_create_new_model:
+            new_contact = Contact(
+                email=contact.email if is_email_nonempty else None,
+                phoneNumber=contact.phoneNumber if is_phone_nonempty else None,
+                linkPrecedence=LinkPrecedence.secondary,
+                linkedId=primary_contact.id
+            )
+            session.add(new_contact)
+            session.commit()
+            session.refresh(new_contact)
+
+        linked_contacts = session.exec(
+            select(Contact).where(
+                or_(
+                    Contact.id == primary_contact.id,
+                    Contact.linkedId == primary_contact.id
+                )
+            )
+        ).fetchall()
+        emails = list({c.email for c in linked_contacts if c.email})
+        phoneNumbers = list({c.phoneNumber for c in linked_contacts if c.phoneNumber})
+        secondaryIds = [c.id for c in linked_contacts if c.linkPrecedence == LinkPrecedence.secondary]
+
+        contact_response = IdentificationModel(
+            primaryContactId=primary_contact.id,
+            emails=emails,
+            phoneNumbers=phoneNumbers,
+            secondaryContactIds=secondaryIds
+        )
+        return {"contact": contact_response}
